f5e82ecd8b7ac68b4b3e3a7d324970fef966436d
mowabanga <itsmundia@gmail.com>
2025-03-18 14:13:35 +0300

    Customise header

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your configuration options here
+  reactStrictMode: true,
+  // other config options...
+};
+
+export default nextConfig;
---
+++ src/components/Pointer.tsx
@@ -0,0 +1,23 @@
+export default function Pointer() {
+    return (
+        <div>
+            <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                width="24" 
+                height="24" 
+                viewBox="0 0 24 24" 
+                fill="none" 
+                stroke="currentColor" 
+                strokeWidth="2" 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                className="feather feather-mouse-pointer text-white"
+            >
+                <path d="M3 3l7.07 16.97 2.51-7.39 7.39-2.51L3 3z"></path><path d="M13 13l6 6"></path>
+            </svg>
+            <div>
+                <div>John</div>
+            </div>
+        </div>
+    )
+}
