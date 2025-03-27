173f5a078481728bfe1659c4c4e866d5031d739a
mowabanga <itsmundia@gmail.com>
2025-03-27 20:26:51 +0300

    Fix typos, add hamburger sidebar with animation

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import quantumLogo from "@/assets/images/quantum.svg";
 import acmeLogo from "@/assets/images/acme-corp.svg";
 import echoValleyLogo from "@/assets/images/echo-valley.svg";
@@ -7,6 +9,8 @@ import apexLogo from "@/assets/images/apex.svg";
 import celestialLogo from "@/assets/images/celestial.svg";
 import twiceLogo from "@/assets/images/twice.svg";
 import Image from "next/image";
+import { Fragment } from "react";
+import { motion } from "framer-motion";
 
 const logos = [
     { name: "Quantum", image: quantumLogo },
@@ -23,19 +27,33 @@ export default function LogoTicker() {
     return (
         <section className="py-24 overflow-x-clip">
             <div className="container">
-                <h3 className="text-center text-white/50 text-xl">Already chosen by these market leaders</h3>
+                <h3 className="flex text-center text-white/50 text-xl">Already chosen by these market leaders</h3>
                 <div className="overflow-hidden mt-12 [mask-image:linera-gradient(to_right, transparent, black_10%, black_90%, transparent));]">
-                    <div className="flex gap-24 pr-24">
-                        {logos.map(logo => (
-                            <Image
-                                key={logo.name}
-                                src={logo.image}
-                                alt={logo.name}
-                                width={150}
-                                height={150}
-                            />
+                    <motion.div 
+                        animate={{
+                            x: "-50%",
+                        }}
+                        transition={{
+                            duration: 20,
+                            ease: 'linear',
+                            repeat: Infinity,
+                        }}
+                        className="flex flex-none gap-24 pr-24"
+                    >
+                        {Array.from({ length: 2 }).map((_, i) => (
+                            <Fragment key={i}> 
+                                {logos.map(logo => ( 
+                                    <Image
+                                        key={logo.name}
+                                        src={logo.image}
+                                        alt={logo.name}
+                                        width={150}
+                                        height={150}
+                                    />
+                                ))}
+                            </Fragment>
                         ))}
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </section>
